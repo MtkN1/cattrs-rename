0ccbab81b252e8f36454c508e9413a403760b8a8
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2025-02-26 21:03:04 +0900

    Project setup

--- src/cattrs_rename/__init__.py
+++ src/cattrs_rename/__init__.py
@@ -1,2 +1,19 @@
-def hello() -> str:
-    return "Hello from cattrs-rename!"
+"""A shorthand syntax for cattrs rename overrides."""
+
+__all__ = [
+    "MetadataKey",
+    "configure_converter",
+    "make_dict_renames",
+    "make_structure_hook_factory",
+    "make_unstructure_hook_factory",
+    "rename",
+]
+
+from cattrs_rename._cols import rename
+from cattrs_rename._gen import (
+    MetadataKey,
+    configure_converter,
+    make_dict_renames,
+    make_structure_hook_factory,
+    make_unstructure_hook_factory,
+)
